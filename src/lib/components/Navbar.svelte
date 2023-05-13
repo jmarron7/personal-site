4e96bafbc95e9c7f61e293581085d5eca3ad7df8
Jesus Marron <jesusmarron7@gmail.com>
2023-05-13 14:37:09 -0500

    redo nav

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,18 +1,29 @@
 <script>
-	import NavbarItem from "./NavbarItem.svelte";
+    import { page } from "$app/stores";
 
-    const routes = ["home", "about", "projects", "contact"]
+    let liClass = "relative"
+    let aClass = "flex h-full items-center p-4 m-4 hover:border-b-2 hover:border-b-primary-500 hover:text-surface-500 hover:bg-primary-900"
+
 </script>
 <header class="bg-background p-4 flex justify-center">
     <nav class="flex justify-center">
-        <div class="relative p-0 m-0 flex justify-center items-center bg-contain h-8 ">
-            {#each routes as route}
-                <NavbarItem route={route}/>
-            {/each}
-        </div>
+        <ul class="relative p-0 m-0 flex justify-center items-center bg-contain h-8 list-none">
+            <li class:active={$page.url.pathname === "/"} class={liClass}>
+                <a class={aClass} href="/">home</a>
+            </li>
+            <li class:active={$page.url.pathname === "/"}>
+                <a class={aClass} href="/about">about</a>
+            </li>
+            <li class:active={$page.url.pathname === "/"}>
+                <a class={aClass} href="/projects">projects</a>
+            </li>
+            <li class:active={$page.url.pathname === "/"}>
+                <a class={aClass} href="/contact">contact</a>
+            </li>
+        </ul>
     </nav>
 </header>
   
-<style>
+<style lange="postcss">
 
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,7 @@
     <meta name="description" content="Jesus is a Fullstack Software Engineer based in Memphis, TN. Read more about me and the services I can help you with."/>
 </svelte:head>
 
-
+Home
 <style>
  
 </style>
